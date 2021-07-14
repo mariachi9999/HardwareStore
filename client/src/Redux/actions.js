39139a3c3b0eae7767e4ae86e18ca8afd3228d57
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-07-14 16:56:35 -0300

    probando vercel

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -29,6 +29,8 @@ import {
 
 import axios from 'axios';
 
+const {REACT_APP_API_HEROKU} = process.env
+
 export const fetchPending = () => ({
 	type: FETCH_PENDING,
 });
@@ -82,7 +84,7 @@ export function getSuggestions(name) {
 
 export function getProducts() {
 	return async (dispatch) => {
-		axios.get('/products/').then((response) => {
+		axios.get(`${REACT_APP_API_HEROKU}/products/`).then((response) => {
 			dispatch({ type: GET_PRODUCTS, payload: response.data });
 		});
 	};
