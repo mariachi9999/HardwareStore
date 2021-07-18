a9b7342d3ad8983fcd912baae1a44caddf4e3242
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-07-18 16:32:09 -0300

    added an authentication button to pass the situation

--- client/src/components/LogIn/LogIn.jsx
+++ client/src/components/LogIn/LogIn.jsx
@@ -13,7 +13,7 @@ import Div from '../StyledComponents/Validation';
 /* global google */
 /* global gapi */
 import { GoogleLogin } from 'react-google-login';
-import {AuthenticationButton} from '../AuthenticationButton/AuthenticationButton'
+import {AuthenticationButton} from '../AuthenticationButton'
 
 const GlobalStyle = createGlobalStyle`
 
