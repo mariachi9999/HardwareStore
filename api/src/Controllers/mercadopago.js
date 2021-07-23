9d4e76635da52b0ea7f7d6e0145d26a8a4264de0
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-07-23 20:34:25 -0300

    fixes

--- api/src/Controllers/mercadopago.js
+++ api/src/Controllers/mercadopago.js
@@ -72,7 +72,7 @@ const createOrder = async function createOrder(req, res) {
 				id: i.prodId,
 			})),
 			back_urls: {
-				success: 'https://e-commerce-copy.vercel.app//webhook',
+				success: 'https://e-commerce-copy.vercel.app/webhook',
 				failure: 'https://ecommerceherni.herokuapp.com/mercadopago/pagos',
 				pending: 'https://ecommerceherni.herokuapp.com/mercadopago/pagos',
 			},
