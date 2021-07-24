64dd025600537fdf44b05f13c68bc942bd0d01c1
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-07-23 21:24:12 -0300

    uploading controller post IPN

--- api/src/Controllers/coinpayment.js
+++ api/src/Controllers/coinpayment.js
@@ -3,6 +3,8 @@ const { response } = require('express');
 const { Sequelize } = require('sequelize');
 const { Order, OrderDetail, Product, User } = require('../db');
 const axios = require('axios');
+const router = Router();
+
 
 
 const { COINPAYMENT_API_PUBLIC, COINPAYMENT_API_SECRET, COINPAYMENT_MERCHAND_ID } = process.env;
@@ -137,6 +139,74 @@ const createOrderCrypto = async function createOrderCrypto(req, res) {
 		res.status(400).json(error);
 	}
 };
+
+//////////////////// Coinpayment IPN
+
+// let 
+//   CoinPayments = require('../lib'),
+//   express      = require('express'),
+//   bodyParser   = require('body-parser');
+    
+// let app = express();
+ 
+// app.use(bodyParser.urlencoded({ extended: true }));
+ 
+let events = CoinPayments.events;
+ 
+let middleware = [
+  CoinPayments.ipn({
+    'merchantId': process.env.COINPAYMENT_MERCHAND_ID,
+    'merchantSecret': process.env.COINPAYMENT_API_SECRET
+  }), 
+  function (req, res, next) {
+    // Handle via middleware
+    console.log(req.body);
+  }]
+ 
+router.use('/', middleware);
+ 
+ 
+ 
+// Handle via instance
+CoinPayments.on('ipn_fail', function(data){
+    // Handle failed transaction
+    console.log("IPN FAIL");
+    console.log(data);
+});
+CoinPayments.on('ipn_pending', function(data){
+    // Handle pending payment
+    console.log("IPN PENDING");
+    console.log(data);
+});
+CoinPayments.on('ipn_complete', function(data){
+    // Handle completed payment
+    console.log("IPN COMPLETE");
+    console.log(data);
+});
+ 
+ 
+// Handle via static field ( can be used in other files, aka no need to init )
+events.on('ipn_fail', function(data){
+    // Handle failed transaction
+    console.log("IPN FAIL");
+    console.log(data);
+});
+events.on('ipn_pending', function(data){
+    // Handle pending payment
+    console.log("IPN PENDING");
+    console.log(data);
+});
+events.on('ipn_complete', function(data){
+    // Handle completed payment
+    console.log("IPN COMPLETE");
+    console.log(data);
+});
+ 
+// app.listen(process.env.PORT || 1337, () => {
+//   console.log("APP listening on port", process.env.PORT || 1337)
+// });
+
+////////////////////
 
 
 module.exports = {
