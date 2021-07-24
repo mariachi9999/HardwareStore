70111a965f76b86eb2cc2f496e62774a75c38871
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-07-23 22:01:00 -0300

    probando ipn

--- api/src/Controllers/coinpayment.js
+++ api/src/Controllers/coinpayment.js
@@ -2,8 +2,6 @@ const Coinpayments = require('coinpayments');
 const { response } = require('express');
 const { Sequelize } = require('sequelize');
 const { Order, OrderDetail, Product, User } = require('../db');
-const axios = require('axios');
-const router = Router();
 
 
 
@@ -151,60 +149,11 @@ const createOrderCrypto = async function createOrderCrypto(req, res) {
  
 // app.use(bodyParser.urlencoded({ extended: true }));
  
-let events = CoinPayments.events;
- 
-let middleware = [
-  CoinPayments.ipn({
-    'merchantId': process.env.COINPAYMENT_MERCHAND_ID,
-    'merchantSecret': process.env.COINPAYMENT_API_SECRET
-  }), 
-  function (req, res, next) {
-    // Handle via middleware
-    console.log(req.body);
-  }]
- 
-router.use('/', middleware);
- 
- 
- 
-// Handle via instance
-CoinPayments.on('ipn_fail', function(data){
-    // Handle failed transaction
-    console.log("IPN FAIL");
-    console.log(data);
-});
-CoinPayments.on('ipn_pending', function(data){
-    // Handle pending payment
-    console.log("IPN PENDING");
-    console.log(data);
-});
-CoinPayments.on('ipn_complete', function(data){
-    // Handle completed payment
-    console.log("IPN COMPLETE");
-    console.log(data);
-});
- 
- 
-// Handle via static field ( can be used in other files, aka no need to init )
-events.on('ipn_fail', function(data){
-    // Handle failed transaction
-    console.log("IPN FAIL");
-    console.log(data);
-});
-events.on('ipn_pending', function(data){
-    // Handle pending payment
-    console.log("IPN PENDING");
-    console.log(data);
-});
-events.on('ipn_complete', function(data){
-    // Handle completed payment
-    console.log("IPN COMPLETE");
-    console.log(data);
-});
- 
-// app.listen(process.env.PORT || 1337, () => {
-//   console.log("APP listening on port", process.env.PORT || 1337)
-// });
+const ipnUpdate = async (req, res)=>{
+	console.log(req)
+	res.json(req)
+}
+
 
 ////////////////////
 
@@ -214,5 +163,6 @@ module.exports = {
 	createTransaction,
 	getTransactionInfo,
     getCoinRates,
-    createOrderCrypto
+    createOrderCrypto,
+	ipnUpdate
 };
